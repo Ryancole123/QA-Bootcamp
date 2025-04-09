eefebc370d37f5138771a7225606d375b2cf7656
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-09 14:12:14 +0100

    JS 02 done

--- QAJSFUND/02_JavaScriptOperators/starter/index.js
+++ QAJSFUND/02_JavaScriptOperators/starter/index.js
@@ -1,28 +1,28 @@
 // Aritmetic Operators
-// console.log(5 + 5);
-// console.log(5 * 10);
-// console.log(10 % 3);
-// console.log(5 + 10 / 2 * 5 - 10);
-// console.log((6 + 10) / 2 * 5 - 10);
+// console.log(5 + 5); 10
+// console.log(5 * 10); 50
+// console.log(10 % 3); 1
+// console.log(5 + 10 / 2 * 5 - 10); 20
+// console.log((6 + 10) / 2 * 5 - 10); 30
 
 // Assignment Operators
-// console.log(x = x + 1);
-// console.log(x+= 1);
-// console.log(x++);
-// console.log(++x);
+// console.log(x = x + 1);1
+// console.log(x+= 1);2
+// console.log(x++);3
+// console.log(++x);4
 
 // Relational Operators
-// console.log(5 > 3);
-// console.log(3 != 3);
-// console.log(3 <= 2 && 5 >2);
-// console.log(!5>3);
+// console.log(5 > 3); true
+// console.log(3 != 3); false
+// console.log(3 <= 2 && 5 >2);true
+// console.log(!5>3);false
 
 // Mismatched Types
-// console.log(5 + "5");
-// console.log(5 + true);
-// console.log(5 * "5");
-// console.log(1 == true);
-// console.log(1 === true);
+// console.log(5 + "5");55
+// console.log(5 + true);6
+// console.log(5 * "5");25
+// console.log(1 == true);true
+// console.log(1 === true);false
 
 
 
