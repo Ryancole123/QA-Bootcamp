0c80213f23fdadc4739222ef3dc9652f22bb8e13
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-09 15:42:32 +0100

    JS 03 completed

--- QAJSFUND/03a_JavaScriptConditionals/starter/index.js
+++ QAJSFUND/03a_JavaScriptConditionals/starter/index.js
@@ -1,2 +1,9 @@
+let age = 18;
+if (age <= 17) { console.log("Underage"); } 
+else if (age <=65) 
+    { console.log("Insurable"); } 
+else { console.log("out of range"); }
 
+
+age <= 17 ? console.log("Underage") : console.log("Of age");
 
