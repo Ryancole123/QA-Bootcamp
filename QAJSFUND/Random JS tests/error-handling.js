71856474fa5c923d6938852741f8da59e5711b55
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-10 12:28:29 +0100

    More error handling

--- QAJSFUND/Random JS tests/error-handling.js
+++ QAJSFUND/Random JS tests/error-handling.js
@@ -1,8 +1,12 @@
 try{
-    let x = parseInt(prompt("Enter a number",""));
+    let x = parseInt(prompt("Enter a number between 1-10",""));
     if(isNaN(x)){
         let e = new Error();
         e.message = "That wasn't a number...";
+        throw e;
+    }if(x < 1 || x > 10){
+        let e = new Error();
+        e.message = "Error. Number out of range";
         throw e;
     }
 }catch (e){
