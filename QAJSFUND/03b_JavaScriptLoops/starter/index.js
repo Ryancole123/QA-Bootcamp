8fb32392e7640faf024a9455a86064dd9a51857d
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-10 10:29:43 +0100

    Loop QuickLab done

---
+++ QAJSFUND/03b_JavaScriptLoops/starter/index.js
@@ -0,0 +1,12 @@
+for (let i = 1; i < 10 ; i++) 
+    { console.log(i); }
+
+let x = 2;
+let loopCounter = 0;
+
+while(x < 10000){
+    x*=x
+    console.log(x);
+    loopCounter++;
+    
+}
--- QAJSFUND/Random JS tests/loop-tests.js
+++ QAJSFUND/Random JS tests/loop-tests.js
@@ -8,3 +8,10 @@ for (let i = 5; i > 0; i--){
     console.log(i);
     
 }
+
+let array = [1,2,3,4,5,6,7,8];
+
+for (let i in array){
+    console.log(`array[${i}] is: ${array[i]}`);
+    
+}
