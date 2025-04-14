435e10744558d9716b603d16cbfcff2c35d41569
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-14 11:50:08 +0100

    Mini calc

--- QAJSFUND/Random JS tests/Shafeeq Buttons/index.js
+++ QAJSFUND/Random JS tests/Shafeeq Buttons/index.js
@@ -1,3 +1,33 @@
 function doSomething(){
     alert("Wagwan");
 }
+
+function london(){
+    alert("i am in LONDON")
+}
+
+function ok(){
+    // const name = document.getElementById("nameInput").value;
+    alert(`Hello there ${document.getElementById("nameInput").value}`)
+    document.getElementById("nameInput").value = "";
+}
+
+function storeSomething(){
+    document.getElementById("nameInput").value = "abc";
+}
+
+function add(){
+    document.getElementById("num3").value = parseInt(document.getElementById("num1").value) + parseInt(document.getElementById("num2").value)  
+}
+
+function subtract(){
+    document.getElementById("num3").value = parseInt(document.getElementById("num1").value) - parseInt(document.getElementById("num2").value)  
+}
+
+function multiply(){
+    document.getElementById("num3").value = parseInt(document.getElementById("num1").value) * parseInt(document.getElementById("num2").value)  
+}
+
+function divide(){
+    document.getElementById("num3").value = (parseFloat(document.getElementById("num1").value) / parseFloat(document.getElementById("num2").value)).toFixed(2)
+}
