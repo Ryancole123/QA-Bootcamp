f4caeb744bb1cc18302f198efd7294fc31ec85ef
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-14 16:33:01 +0100

    Events and buttons etc

--- QAJSFUND/Random JS tests/Shafeeq Buttons/index.js
+++ QAJSFUND/Random JS tests/Shafeeq Buttons/index.js
@@ -17,17 +17,33 @@ function storeSomething(){
 }
 
 function add(){
+    if(document.getElementById("num1").value && document.getElementById("num2").value){
     document.getElementById("num3").value = parseInt(document.getElementById("num1").value) + parseInt(document.getElementById("num2").value)  
+    }else{
+        alert("Please ensure that all boxes are filled")
+    }
 }
 
 function subtract(){
+    if(document.getElementById("num1").value && document.getElementById("num2").value){
     document.getElementById("num3").value = parseInt(document.getElementById("num1").value) - parseInt(document.getElementById("num2").value)  
+    }else{
+        alert("Please ensure that all boxes are filled")
+    }
 }
 
 function multiply(){
+    if(document.getElementById("num1").value && document.getElementById("num2").value){
     document.getElementById("num3").value = parseInt(document.getElementById("num1").value) * parseInt(document.getElementById("num2").value)  
+    }else{
+        alert("Please ensure that all boxes are filled")
+    }
 }
 
 function divide(){
+    if(document.getElementById("num1").value && document.getElementById("num2").value){
     document.getElementById("num3").value = (parseFloat(document.getElementById("num1").value) / parseFloat(document.getElementById("num2").value)).toFixed(2)
+    }else{
+        alert("Please ensure that all boxes are filled")
+    }
 }
