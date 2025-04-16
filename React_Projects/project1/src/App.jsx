64af90c1179a7024975a0a4278ad251c356f40de
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-16 09:47:20 +0100

    Day2

--- React_Projects/project1/src/App.jsx
+++ React_Projects/project1/src/App.jsx
@@ -2,18 +2,22 @@ import SalarySlip from "./components/HRComponents/SalarySlip"
 import Account from "./components/AccountsComponents/Account"
 import SalarySlip1 from "./components/HRComponents/SalarySlip1"
 import ButtonComponent from "./components/ButtonComponent"
+import Data from "./components/Data"
 
 function App() {
 
   return (
-    <div>
-      <h1>Employee Information System</h1>
-      <SalarySlip ename="James Bond" address="London, UK" salary="20000"/>
-      <SalarySlip ename="Robb Stark" address="Winterfell" salary="10"/>
-      <SalarySlip1 ename="Walter White" address="Aberquerque" salary="800000"/>
-      <Account/>
-      {/* <ButtonComponent/> */}
-    </div>
+    // <div>
+    //   <h1>Employee Information System</h1>
+    //   <SalarySlip ename="James Bond" address="London, UK" salary="20000"/>
+    //   <SalarySlip ename="Robb Stark" address="Winterfell" salary="10"/>
+    //   <SalarySlip1 ename="Walter White" address="Aberquerque" salary="800000"/>
+    //   <Account/>
+    //   {/* <ButtonComponent/> */}
+    // </div>
+
+
+    <Data/>
 
   )
 }
---
+++ React_Projects/project1/src/components/Data.jsx
@@ -0,0 +1,15 @@
+
+
+
+let Data = () =>{
+
+    let students = ["Shafeeq", "John", "James", "Peter", "Ryan"]
+
+    return(
+        <>
+        <h2>List of Students</h2>
+        
+        </>
+    )
+}
+export default Data
