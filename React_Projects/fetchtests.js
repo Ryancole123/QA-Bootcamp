3d4f0843c4cf282b99eb600206232cae9b93c9cf
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-17 16:11:17 +0100

    JSON server + fetch test

--- React_Projects/fetchtests.js
+++ React_Projects/fetchtests.js
@@ -1,10 +1,10 @@
 
 
-let object1 = fetch("https://jsonplaceholder.typicode.com/posts")
+let object1 = fetch("http://localhost:3000/seller")
 object1.then((response) => {
             if(response.status==200){
                 let object2 = response.json()
-                object2.then((records) => records.map((rec) => console.log(rec))                )
+                object2.then((records) => records.map((rec) => console.log(rec.firstName))                )
             }})
 
 
